b6a562a497dac08a276a0e048e9cb94beb27e317
赵晨光 <596094101@qq.com>
2019-07-27 17:00:56 +0800

    vue init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld
+      v-bind:propFunc="{
+        name: 'Veronica',
+        company: 'Veridian Dynamics'
+      }"
+      msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      
+    }
+  },
+  props: {
+    msg: 'String',
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <a-button @click="doSomeThing" type="primary">Primary</a-button>
+    <a-button>Default{{ msg }}</a-button>
+    <a-button type="dashed">Dashed"{{ test }}"</a-button>
+    <a-button type="danger">Danger</a-button>
+  </div>
+</template>
+<script>
+import Vue from 'vue';
+import { Button } from 'ant-design-vue';
+Vue.use(Button);
+// doSomething = () => {
+//   console.log(11111);
+// }
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      test: 'test',
+    }
+  },
+  methods: {
+    doSomeThing() {
+      console.log(this.propFunc);
+      this.$data.test = this.propFunc.name;
+    }
+  },
+  props: {
+    msg: String,
+    propFunc: {
+      type: Object,
+      default: function () {
+        return { propFunc }
+      }
+    },
+  }
+}
+</script>
